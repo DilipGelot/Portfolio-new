b6ffb100e250697901abc6a40d2c3156cddf5d3e
DilipGelot <dilipgelot16@gmail.com>
2024-03-29 10:28:43 +0530

    City Update

--- js/script.js
+++ js/script.js
@@ -1,98 +1,91 @@
 // Scroll Event
-window.addEventListener("scroll", function(){
-if(this.window.pageYOffset>50)
-{
-    document.getElementById("header").style.boxShadow  = "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px";
-}
-else{
-    document.getElementById("header").style.boxShadow  = "none";
-}
-})
-
+window.addEventListener("scroll", function () {
+  if (this.window.pageYOffset > 50) {
+    document.getElementById("header").style.boxShadow =
+      "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px";
+  } else {
+    document.getElementById("header").style.boxShadow = "none";
+  }
+});
 
 // Humburger Menu  ***********************
 
 var icon = document.getElementById("humburger_icon");
-var menuBox = document.getElementById("menu-box");  
-icon.onclick = function(){
-    menuBox.classList.toggle("open-menu")
-}
+var menuBox = document.getElementById("menu-box");
+icon.onclick = function () {
+  menuBox.classList.toggle("open-menu");
+};
 
 // Typing Text    ************************
 
-
-const words = ["Frontend Developer","React JS Developer", "Web Designer"];
+const words = ["Frontend Developer", "React JS Developer", "Web Designer"];
 let j = 0;
 let timer;
 
 function typingEffect() {
-	let word = words[j].split("");
-	var loopTyping = function() {
-		if (word.length > 0) {
-			document.getElementById('word').innerHTML += word.shift();
-		} else {
-			deletingEffect();
-			return false;
-		};
-		timer = setTimeout(loopTyping, 200);
-	};
-	loopTyping();
-};
-
+  let word = words[j].split("");
+  var loopTyping = function () {
+    if (word.length > 0) {
+      document.getElementById("word").innerHTML += word.shift();
+    } else {
+      deletingEffect();
+      return false;
+    }
+    timer = setTimeout(loopTyping, 200);
+  };
+  loopTyping();
+}
 
 function deletingEffect() {
-	let word = words[j].split("");
-	var loopDeleting = function() {
-		if (word.length > 0) {
-			word.pop();
-			document.getElementById('word').innerHTML = word.join("");
-		} else {
-			if (words.length > (j + 1)) {
-				j++;
-			} else {
-				j = 0;
-			};
-			typingEffect();
-			return false;
-		};
-		timer = setTimeout(loopDeleting, 100);
-	};
-	loopDeleting();
-};
+  let word = words[j].split("");
+  var loopDeleting = function () {
+    if (word.length > 0) {
+      word.pop();
+      document.getElementById("word").innerHTML = word.join("");
+    } else {
+      if (words.length > j + 1) {
+        j++;
+      } else {
+        j = 0;
+      }
+      typingEffect();
+      return false;
+    }
+    timer = setTimeout(loopDeleting, 100);
+  };
+  loopDeleting();
+}
 
 typingEffect();
 
-
-
-
-
-
-
-//  Project Slider            ******************************************** 
+//  Project Slider            ********************************************
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlide(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 function currentslide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
   }
 
-  slides[slideIndex-1].style.display = "block";  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
 
+  slides[slideIndex - 1].style.display = "block";
 }
 
 var slideIndex = 0;
@@ -100,38 +93,34 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.getElementsByClassName("slide");
-  for (i = 0; i < slides.length; i++){
+  for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length){
+  if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  slides[slideIndex - 1].style.display = "block"
+  slides[slideIndex - 1].style.display = "block";
 }
-
-
-
 
 // ************   Contact Form        ******************
 
 let msg_box = document.getElementById("contact_form");
 
-document.getElementById("connect").onclick = function(){
-	msg_box.style.display = "flex";
-}
-document.getElementById("close").onclick = function(){
-	msg_box.style.display = "none";
-}
+document.getElementById("connect").onclick = function () {
+  msg_box.style.display = "flex";
+};
+document.getElementById("close").onclick = function () {
+  msg_box.style.display = "none";
+};
 
- // ************ Dark mode *********
+// ************ Dark mode *********
 
- const chk = document.getElementById('chk');
+const chk = document.getElementById("chk");
 
-chk.addEventListener('change', () => {
-	document.body.classList.toggle('dark');
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("dark");
 });
-
 
 // ************ Weather *********
 
@@ -144,39 +133,41 @@ const options = {
     "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
   },
 };
-const getWeather = (city)=>{
-    cityName.innerHTML = city;
-    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13?city='+city, options)
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        wind_kph.innerHTML = response.current.wind_kph;
-        pressure_in.innerHTML = response.current.pressure_in;
-        humidity.innerHTML = response.current.humidity;
-        cloud.innerHTML = response.current.cloud;
-        feelslike_c.innerHTML = response.current.feelslike_c;
-        uv.innerHTML = response.current.uv;
-        temp_c.innerHTML = response.current.temp_c;
-        cityName.innerHTML = response.location.name;
-        time.innerHTML = response.location.localtime;
-        country.innerHTML = response.location.country;
-        region.innerHTML = response.location.region;
-        tz_id.innerHTML = response.location.tz_id;
-        text.innerHTML = response.current.condition.text;
+const getWeather = (city) => {
+  cityName.innerHTML = city;
+  fetch(
+    "https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13?city=" +
+      city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      wind_kph.innerHTML = response.current.wind_kph;
+      pressure_in.innerHTML = response.current.pressure_in;
+      humidity.innerHTML = response.current.humidity;
+      cloud.innerHTML = response.current.cloud;
+      feelslike_c.innerHTML = response.current.feelslike_c;
+      uv.innerHTML = response.current.uv;
+      temp_c.innerHTML = response.current.temp_c;
+      cityName.innerHTML = response.location.name;
+      time.innerHTML = response.location.localtime;
+      country.innerHTML = response.location.country;
+      region.innerHTML = response.location.region;
+      tz_id.innerHTML = response.location.tz_id;
+      text.innerHTML = response.current.condition.text;
     })
     .catch((err) => console.log(err));
-}
+};
 submits.addEventListener("click", (e) => {
-    e.preventDefault();
-    if(city.value.length == 0){
-        alert("Please Enter a City Name");
-    }
-    else{
+  e.preventDefault();
+  if (city.value.length == 0) {
+    alert("Please Enter a City Name");
+  } else {
+    getWeather(city.value);
+    city.value = "";
+    app.style.opacity = "0";
+  }
+});
 
-        getWeather(city.value)
-        city.value="";
-        app.style.opacity="0";
-    }
-})
-
-getWeather("Ahmedabad");
+getWeather("Ahmedabad gujarat");
